68626e73ad446e708abccf2919c152738dc31e63
HanSungduk <126736559+HanSungduk@users.noreply.github.com>
2023-08-04 11:12:52 +0900

    Add files via upload

--- work/gradio_test.py
+++ work/gradio_test.py
@@ -1,9 +1,9 @@
 import numpy as np
 import gradio as gr
-import oemer.ete as ete
 import os
 import sys as _sys
 
+import oemer.ete as ete
 from oemer import MODULE_PATH
 from oemer.utils import get_logger
 import argparse
@@ -12,53 +12,74 @@ import music21
 from music21 import *    #pip install music21
 
 
-def oemer(input_img):
-    # parser = ete.get_parser() # 명령줄 인수를 파싱하는 파서 객체 생성
-    # args = parser.parse_args() # 명령줄 인수를 파싱하여 가져옴
-    args = argparse.Namespace(img_path=input_img, output_path='./', use_tf=False, save_cache=False, without_deskew=False)
+def oemer( input_img, input_name ) :
+    print( "input_img  : " + input_img )
+    print( "input_name : " + input_name )
 
-    print("args.img_path:" + args.img_path)
+    # 입력받은 image의 file명을 입력받은 text로 변경
+    input_name_path = input_img.replace( "image", input_name )
+    print( "input_name_path : " + input_name_path )
 
-    if not os.path.exists(args.img_path):
-        raise FileNotFoundError(f"The given image path doesn't exists: {args.img_path}") # 지정된 이미지 경로가 존재하지 않으면 예외 발생
+    from shutil import copyfile
+    # 입력받은 text명으로 복사 생성
+    copyfile( input_img, input_name_path )
+
+    # ete.extract()를 호출하기 위한 argument 생성
+    args = argparse.Namespace( img_path=input_name_path, output_path='./', use_tf=False, save_cache=False, without_deskew=False )
+
+    print( "args.img_path : " + args.img_path )
+
+    # 지정된 이미지 경로가 존재하지 않으면 예외 발생
+    if not os.path.exists( args.img_path ) :
+        raise FileNotFoundError( f"The given image path doesn't exists: {args.img_path}" )
 
     # Check there are checkpoints
-    chk_path = os.path.join(MODULE_PATH, "checkpoints/unet_big/model.onnx")
-    if not os.path.exists(chk_path):
-        ete.logger.warn("No checkpoint found in %s", chk_path) # 체크포인트가 없는 경우 경고 메시지 출력
-        for idx, (title, url) in enumerate(ete.CHECKPOINTS_URL.items()):
-            ete.logger.info(f"Downloading checkpoints ({idx+1}/{len(ete.CHECKPOINTS_URL)})") # 체크포인트 다운로드 중인 경우 로그 메시지 출력 
-            save_dir = "unet_big" if title.startswith("1st") else "seg_net" # 체크포인트 저장 디렉토리 결정
-            save_dir = os.path.join(MODULE_PATH, "checkpoints", save_dir)
-            save_path = os.path.join(save_dir, title.split("_")[1]) # 체크포인트 저장 경로 결정
-            ete.download_file(title, url, save_path) # 체크포인트 파일 다운로드 함수 호출
+    chk_path = os.path.join( MODULE_PATH, "checkpoints/unet_big/model.onnx" )
+    if not os.path.exists( chk_path ) :
+        # 체크포인트가 없는 경우 경고 메시지 출력
+        ete.logger.warn( "No checkpoint found in %s", chk_path )
+        for idx, ( title, url ) in enumerate( ete.CHECKPOINTS_URL.items() ) :
+            # 체크포인트 다운로드 중인 경우 로그 메시지 출력 
+            ete.logger.info( f"Downloading checkpoints ( {idx + 1} / {len( ete.CHECKPOINTS_URL )})" ) 
+            # 체크포인트 저장 디렉토리 결정
+            save_dir = "unet_big" if title.startswith( "1st" ) else "seg_net" 
+            save_dir = os.path.join( MODULE_PATH, "checkpoints", save_dir )
+            # 체크포인트 저장 경로 결정
+            save_path = os.path.join( save_dir, title.split( "_" )[1] )
+            # 체크포인트 파일 다운로드 함수 호출
+            ete.download_file( title, url, save_path ) 
 
-    ete.clear_data() # 이전에 등록된 레이어 데이터 모두 삭제
-    mxl_path = ete.extract(args) # 이미지에서 악보 추출하여 MusicXML 생성 및 반환
-    print("mxl_path:" + mxl_path)
-    # img = ete.teaser() # 악보 이미지에 바운딩 박스 등 시각화 정보 추가
-    # img.save(mxl_path.replace(".musicxml", "_teaser.png")) # 시각화한 이미지 저장
+    # 이전에 등록된 레이어 데이터 모두 삭제
+    ete.clear_data() 
+    # 이미지에서 악보 추출하여 MusicXML 생성 및 반환
+    mxl_path = ete.extract( args ) 
+    print( "mxl_path : " + mxl_path )
+    # 악보 이미지에 바운딩 박스 등 시각화 정보 추가
+    img = ete.teaser() 
+    # 시각화한 이미지 저장
+    img.save( mxl_path.replace( ".musicxml", "_teaser.png" ) ) 
 
-    # mxl_path = "./image.musicxml"
+    # 점자 변환 : 화면에 보여주기 위한
+    txt_path = mxl_path.replace( ".musicxml", ".txt" )
+    print( "txt_path : " + txt_path )
+    t = music21.converter.parse( mxl_path ).write( "braille", fp=txt_path )
 
-    # # 점자 변환
-    # from music21 import braille
-    # dataStr = braille.translate.objectToBraille(mxl_path)
+    # 점자 변환 : 파일 다운로드용
+    brf_path = mxl_path.replace( ".musicxml", ".brf" )
+    print( "brf_path : " + brf_path )
+    s = music21.converter.parse( mxl_path ).write( "braille.ascii", fp=brf_path )
+ 
+    # 파일을 읽어 온다
+    view_brf = ""
+    with open( txt_path, 'rt', encoding='utf-8' ) as f :
+        view_brf = f.read()
 
-    
-    brf_path = mxl_path.replace(".musicxml", ".brf")
-    print("brf_path:" + brf_path)
-    s = music21.converter.parse( mxl_path ).write( "braille", fp=brf_path )
-
-    dataStr = ""
-    with open(brf_path, 'rt', encoding='utf-8') as f:
-        dataStr = f.read()
-
-
-    # dataStr = s.read_text
-
-    return dataStr
+    return view_brf, brf_path
 
 
-demo = gr.Interface(oemer, gr.Image(type="filepath",shape=(300, 600)), "text").queue()
+demo = gr.Interface(
+    fn=oemer,
+    inputs=[gr.Image( type="filepath" ), "text"],
+    outputs=["text", "file"],
+).queue() # timeout error 방지를 위해 
 demo.launch()
